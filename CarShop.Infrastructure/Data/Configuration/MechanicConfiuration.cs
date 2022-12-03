03e60046921dc5f287a590bc20c0f4c491a26c09
Sinan Mehmed <sinan00313@gmail.com>
2022-12-03 02:08:17 +0000

    Mechanics Configuration Added

---
+++ CarShop.Infrastructure/Data/Configuration/MechanicConfiuration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Infrastructure.Data.Configuration
+{
+    public class MechanicConfiuration : IEntityTypeConfiguration<Mechanic>
+    {
+        public void Configure(EntityTypeBuilder<Mechanic> builder)
+        {
+            builder.HasData(CreateMechanics());
+        }
+
+        private List<Mechanic> CreateMechanics()
+        {
+            var mechanics = new List<Mechanic>()
+            {
+                new Mechanic()
+                 {
+                      Id = 1,
+                      FirstName = "Pesho",
+                      LastName = "Petrov",
+                      Rating = 10.00M                                       
+                     
+                 },
+
+                new Mechanic()
+                 {
+                      Id = 2,
+                      FirstName = "Gosho",
+                      LastName = "Petkov",
+                      Rating = 8.25M
+
+                 },
+
+                new Mechanic()
+                 {
+                      Id = 3,
+                      FirstName = "Ivelin",
+                      LastName = "Radev",
+                      Rating = 9.60M
+
+                 },
+            };
+
+            return mechanics;
+        }
+    }
+}
