d365901dede053cd1287ff6d3b276dfec64d2c18
Sinan Mehmed <sinan00313@gmail.com>
2022-12-16 19:30:01 +0000

    CarController Added

---
+++ CarShop/Controllers/CarController.cs
@@ -0,0 +1,80 @@
+﻿using CarShop.Core.Models.Car;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Controllers
+{
+    [Authorize]
+    public class CarController : Controller
+    {
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var model = new CarsQueryModel();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Mine()
+        {
+            var model = new CarsQueryModel();
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = new CarDetailsModel();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CarModel model)
+        {
+            int id = 1;
+
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = new CarModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, CarModel model)
+        {
+            
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Buy(int id)
+        {
+
+            return RedirectToAction(nameof(Mine));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Sell(int id)
+        {
+
+            return RedirectToAction(nameof(Mine));
+        }
+    }
+}
