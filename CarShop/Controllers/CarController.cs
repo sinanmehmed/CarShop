54224982e4b4ae31025f52d8e070d52346a6be75
Sinan Mehmed <sinan00313@gmail.com>
2022-12-20 15:26:32 +0000

    GetMoethod added in CarController

--- CarShop/Controllers/CarController.cs
+++ CarShop/Controllers/CarController.cs
@@ -22,6 +22,7 @@ namespace CarShop.Controllers
             dealerService = _dealerService;
         }
 
+        [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> All([FromQuery]AllCarsQueryModel query)
         {
